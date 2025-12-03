c64ab9ad8845a6aab4cc09ddf59c2ea466717ba9
surajacharya12 <surajacharya.080@kathford.edu.np>
2025-12-03 20:30:27 +0545

    fix

--- app/api/url.ts
+++ app/api/url.ts
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://eagle-backend-al9s5828y-suraj-acharyas-projects-0bbb95ae.vercel.app"
+    ? "https://eagle-backend-suraj-acharyas-projects-0bbb95ae.vercel.app"
     : "http://localhost:3001";
 
 export default API_URL;
